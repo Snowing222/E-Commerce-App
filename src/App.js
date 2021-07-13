12526d39e9aecdf188a8893feea1804ca6d006bd
snowing222 <snowing222@gmail.com>
2021-07-13 16:07:46 -0400

    implement react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react'
 // import Navbar from './components/Navbar/Navbar'
 import {commerce} from './lib/commerce'
 import {Products, Navbar, Cart} from './components'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 
 const App = () => {
@@ -32,11 +33,19 @@ const App = () => {
     console.log(cart)
 
     return (
-        <div>
-            <Navbar totalItems = {cart.total_items} />
-            {/* <Products products = {products} onAddToCart = {handleAddToCart} />         */}
-            <Cart cart = {cart} />
-        </div>
+        <Router>
+            <div>
+                <Navbar totalItems = {cart.total_items} />
+                <Switch>
+                    <Route exact path = "/">
+                        <Products products = {products} onAddToCart = {handleAddToCart} />  
+                    </Route>
+                    <Route>
+                    <Cart cart = {cart} />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,32 @@
 import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
+import {Link, useLocation} from 'react-router-dom'
 
 import useStyles from './styles'
 
 const Navbar = ({totalItems}) => {
     const classes = useStyles()
+    const location = useLocation()
+
     
     return (
         <>
             <AppBar position = "fixed" className = {classes.appBar} color = "inherit" >
                 <Toolbar>
-                    <Typography variant = "h6" className={classes.title} color = "inherit">
+                    <Typography component = {Link}  to = '/' variant = "h6" className={classes.title} color = "inherit">
                         <img src = "https://freepikpsd.com/media/2019/10/shop-png-icon-1-Transparent-Images.png" alt = "Commerce.js" height = "25px" className = {classes.imgage} />
                         Emazon
                     </Typography>
                     <div className = {classes.grow} />
+                    {location.pathname === '/' && (
                     <div className = {classes.button}>
-                        <IconButton aria-label = "Show cart items" color = "inherit">
+                        <IconButton component = {Link} to ='/cart' aria-label = "Show cart items" color = "inherit">
                             <Badge badgeContent = {totalItems} color = "secondary"></Badge>
                                 <ShoppingCart />
                         </IconButton>
 
-                    </div>
+                    </div>)}
                 </Toolbar>
             </AppBar>
             
